ae70143ce4e66c19da2d717a13c45694127116b5
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-03-13 22:45:50 +0300

    refactor: upload blob to firebase

--- server/src/utils/upload.ts
+++ server/src/utils/upload.ts
@@ -68,3 +68,16 @@ export class Upload {
     }
   }
 }
+
+
+
+// //upload blob or file directly from javascript File
+// import { getStorage, ref, uploadBytes } from "firebase/storage";
+
+// const storage = getStorage();
+// const storageRef = ref(storage, 'some-child');
+
+// // 'file' comes from the Blob or File API
+// uploadBytes(storageRef, file).then((snapshot) => {
+//   console.log('Uploaded a blob or file!');
+// });
