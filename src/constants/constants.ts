9a7bbce7ae411df5f255b8a7c0e47f692dff866a
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-04-19 16:53:32 +0300

    First deploy to netlify

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,7 +3,7 @@ let URI: string;
 if (process.env.NODE_ENV === "development") {
   URI = "http://localhost:8000/graphql";
 } else {
-  URI = "some_base_production_url/graphql";
+  URI = "https://archivo-backend.onrender.com";
 }
 
 export { URI };
